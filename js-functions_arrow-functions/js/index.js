8a55988d524144edc748b5f47588c1a8919fadc3
Margeaux Gebert <margeaux.gebert@gmail.com>
2025-02-14 20:15:54 +0100

    commit solution

--- js-functions_arrow-functions/js/index.js
+++ js-functions_arrow-functions/js/index.js
@@ -7,7 +7,7 @@ Use implicit returns when possible.
 HINT: You can find more information about arrow functions and implicit returns in the handout.
 */
 
-function getCurrentHour() {
+const getCurrentHour = () => {
   const now = new Date();
   const currentHour = now.getHours();
   if (currentHour === 0) {
@@ -19,21 +19,25 @@ function getCurrentHour() {
   } else {
     return currentHour - 12 + "pm";
   }
-}
+};
+console.log("What time is it? " + getCurrentHour());
 
-function getVectorLength(x, y, z) {
+const getVectorLength = (x, y, z) => {
   return (x ** 2 + y ** 2 + z ** 2) ** 0.5;
-}
+};
 
-function cleanInput(string) {
+console.log("The vector lenght is: " + getVectorLength(1, 4, 7).toFixed(2));
+
+const cleanInput = (string) => {
   return string.toLowerCase().trim();
-}
+};
+console.log("it's not written in uppercase: " + cleanInput("MANGO"));
 
 /*
 Rewrite the following arrow functions as classic functions.
 */
 
-const isOddAndSmall = (number) => {
+function isOddAndSmall(number) {
   if (number > 10) {
     return false;
   }
@@ -43,7 +47,8 @@ const isOddAndSmall = (number) => {
   }
 
   return true;
-};
+}
+console.log(`It's ${isOddAndSmall(5)}!`);
 
 const add3 = (a, b, c) => a + b + c;
 
