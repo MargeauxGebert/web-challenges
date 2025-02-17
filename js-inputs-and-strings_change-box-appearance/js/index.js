c1d955c7bdce1b68568d2ec4c2743c6594d230e2
Margeaux Gebert <margeaux.gebert@gmail.com>
2025-02-17 18:24:26 +0100

    solution commit

--- js-inputs-and-strings_change-box-appearance/js/index.js
+++ js-inputs-and-strings_change-box-appearance/js/index.js
@@ -1,1 +1,31 @@
 console.clear();
+
+function getElement(datajs) {
+  return document.querySelector(`[data-js=${datajs}]`);
+}
+
+const box = getElement("box");
+const inputColor = getElement("input-color");
+const inputRadius = getElement("input-radius");
+const inputRotation = getElement("input-rotation");
+
+inputColor.addEventListener("input", () => {
+  const hue = inputColor.value;
+  const colorString = `hsl(${hue}deg, 50%, 50%)`;
+
+  box.style.backgroundColor = colorString;
+});
+
+inputRadius.addEventListener("input", () => {
+  const radius = inputRadius.value;
+  const radiusString = `${radius}%`;
+
+  box.style.borderRadius = radiusString;
+});
+
+inputRotation.addEventListener("input", () => {
+  const rotation = inputRotation.value;
+  const rotationString = `rotate(${rotation}deg)`;
+
+  box.style.transform = rotationString;
+});
