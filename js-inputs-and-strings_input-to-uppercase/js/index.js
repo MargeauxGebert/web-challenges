517f4d75c6524e7edee6493385a114f53fa53182
Margeaux Gebert <margeaux.gebert@gmail.com>
2025-02-17 15:41:27 +0100

    solution commit

--- js-inputs-and-strings_input-to-uppercase/js/index.js
+++ js-inputs-and-strings_input-to-uppercase/js/index.js
@@ -1,1 +1,17 @@
 console.clear();
+
+const firstInput = document.querySelector('[data-js="first-input"]');
+const buttonUppercase = document.querySelector('[data-js="button-uppercase"]');
+
+buttonUppercase.addEventListener("click", () => {
+  firstInput.value = firstInput.value.toUpperCase();
+});
+
+// console.clear();
+
+// const input = document.querySelector('[data-js="first-input"]');
+// const button = document.querySelector('[data-js="button-uppercase"]');
+
+// button.addEventListener("click", () => {
+//   input.value = input.value.toUpperCase();
+// });
