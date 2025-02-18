d324ccd17f04f29ba2c517c02f4bb0b6a131b265
Margeaux Gebert <margeaux.gebert@gmail.com>
2025-02-18 12:49:01 +0100

    solution commit

--- js-forms_handle-form-submit/js/index.js
+++ js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,21 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+  console.log(data);
+
+  console.log(
+    `The age-badness-sum of ${data.firstName} is ${
+      Number(data.age) + Number(data.badness)
+    }`
+  );
+
+  event.target.reset();
+
+  event.target.elements.firstName.focus();
+});
