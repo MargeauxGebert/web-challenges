fc5483d3bf691f30e1cb96920124ed26825cf7c1
Margeaux Gebert <margeaux.gebert@gmail.com>
2025-02-18 18:47:50 +0100

    add solution challenge calculator

--- js-forms_calculator/js/index.js
+++ js-forms_calculator/js/index.js
@@ -25,7 +25,20 @@ form.addEventListener("submit", (event) => {
   let result;
 
   // --v-- write your code here --v--
-
+  const numberA = form.elements.numberA.value;
+  const numberB = form.elements.numberB.value;
+  // const operatorAdd = form.elements.addition.checked;
+  // console.log(operatorAdd);
+  if (form.elements.addition.checked === true) {
+    // const resultAdd = add(Number(numberA), Number(numberB));
+    result = add(Number(numberA), Number(numberB));
+  } else if (form.elements.subtraction.checked === true) {
+    result = subtract(Number(numberA), Number(numberB));
+  } else if (form.elements.multiplication.checked === true) {
+    result = multiply(Number(numberA), Number(numberB));
+  } else {
+    result = divide(Number(numberA), Number(numberB));
+  }
   // --^-- write your code here --^--
 
   resultOutput.textContent = result;
