95c2b902e23231363674dc502781441235a88e74
Margeaux Gebert <margeaux.gebert@gmail.com>
2025-02-18 18:14:59 +0100

    add solution to challenge checkbox

--- js-forms_checkbox-input/js/index.js
+++ js-forms_checkbox-input/js/index.js
@@ -3,6 +3,7 @@ console.clear();
 const form = document.querySelector('[data-js="form"]');
 const tosError = document.querySelector('[data-js="tos-error"]');
 const tosCheckbox = document.querySelector('[data-js="tos"]');
+const successMessage = document.querySelector('[data-js="success"]');
 
 function hideTosError() {
   tosError.setAttribute("hidden", "");
@@ -16,14 +17,19 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   // --v-- write your code here --v--
-  if (tosCheckbox.ckecked === false) {
+  if (tosCheckbox.checked === false) {
     showTosError();
     return;
   } else {
     hideTosError();
   }
+  successMessage.removeAttribute("hidden");
   // --^-- write your code here --^--
 
   // eslint-disable-next-line no-alert
   alert("Form submitted");
+});
+
+tosCheckbox.addEventListener("change", () => {
+  hideTosError();
 });
