dd08dee5975c946cb9d3b4ebc506f89790d206ef
Margeaux Gebert <margeaux.gebert@gmail.com>
2025-02-18 17:25:28 +0100

    add two solutions to challenge three

--- js-createelement_product-teaser/js/index copy.js
+++ js-createelement_product-teaser/js/index copy.js
@@ -11,38 +11,36 @@ const imageSrc =
   "https://unsplash.com/photos/3VOTHTrE614/download?ixid=MnwxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNjU5NTM3NTA2&force=true&w=640";
 
 const newProduct = document.createElement("article");
-newProduct.classList.add("product");
+const newProductBody = document.createElement("section");
+const newProductTextContainer = document.createElement("div");
+const newProductImageContainer = document.createElement("div");
 const newProductFooter = document.createElement("footer");
-newProductFooter.classList.add("product__footer");
 const newProductBuyButton = document.createElement("button");
+
+newProduct.classList.add("product");
+newProductBody.classList.add("product__body");
+newProductTextContainer.classList.add("product__text-container");
+newProductImageContainer.classList.add("product__image-container");
+newProductFooter.classList.add("product__footer");
 newProductBuyButton.classList.add("product__buy-button");
 
-newProduct.innerHTML = `<section class="product__body">
-        <div class="product__text-container">
-          <h2 class="product__name">${name}</h2>
-          <ul class="product__categories">
-            <li class="product__category">${category1}r</li>
-            <li class="product__category">${category2}</li>
-            <li class="product__category">${category3}</li>
-          </ul>
-          <p class="product__description">
-          ${description}
-          </p>
-        </div>
-        <div class="product__image-container">
-          <img
-            class="product__image"
-            src=${imageSrc}
-            alt=""
-          />
-        </div>
-      </section>`;
+newProductTextContainer.innerHTML = `<h2 class="product__name">${name}</h2>
+  <ul class="product__categories">
+  <li class="product__category">${category1}</li>
+  <li class="product__category">${category2}</li>
+  <li class="product__category">${category3}</li></ul>
+  <p class="product__description"> ${description}</p>`;
+
+newProductImageContainer.innerHTML = `<img class="product__image" src=${imageSrc}/>`;
 
 newProductFooter.innerHTML = `<span class="product__price">${price}</span>`;
 
 newProductBuyButton.textContent = "Buy";
 
 document.body.append(newProduct);
+newProduct.append(newProductBody);
+newProductBody.append(newProductTextContainer);
+newProductBody.append(newProductImageContainer);
 newProduct.append(newProductFooter);
 newProductFooter.append(newProductBuyButton);
 
