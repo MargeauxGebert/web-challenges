1d1bec3aa90a918064b867daa343c35d00a664ae
Margeaux Gebert <margeaux.gebert@gmail.com>
2025-02-20 10:58:26 +0100

    add solution two

--- js-loops_forof-list/js/index.js
+++ js-loops_forof-list/js/index.js
@@ -16,5 +16,9 @@ const programmingLanguages = [
 ];
 
 // --v-- write or modify code below this line --v--
-
+for(const listItems of programmingLanguages) {
+  const li = document.createElement("li");
+  ol.append(li);
+  li.textContent = listItems;
+}
 // --^-- write or modify code above this line --^--
