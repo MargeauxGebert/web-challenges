2ad0535ce6599d1555c1931f7bb19026312d52bd
Margeaux Gebert <margeaux.gebert@gmail.com>
2025-02-20 12:47:22 +0100

    commit solution while

--- js-loops_while-random-number/index.js
+++ js-loops_while-random-number/index.js
@@ -4,7 +4,12 @@ let number = 0;
 let count = 0;
 
 // --v-- write or modify code below this line --v--
+while (number <= 0.9) {
+  count++;
+  number = Math.random();
+  console.log(number);
 
+}
 // --^-- write or modify code above this line --^--
 
 console.log(
