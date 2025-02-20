e01947f0d1cd775178c57b2e85db7f2da9f3dabc
Margeaux Gebert <margeaux.gebert@gmail.com>
2025-02-20 11:44:37 +0100

    add solution stars part one

--- js-loops_stars/js/index.js
+++ js-loops_stars/js/index.js
@@ -7,7 +7,11 @@ function renderStars() {
   starContainer.innerHTML = "";
 
   // --v-- write or modify code below this line --v--
-
+for(let starValue = 1; starValue <=5; starValue++) {
+  const starImg = document.createElement("img");
+  starImg.setAttribute("src", "../assets/star-empty.svg");
+  starContainer.append(starImg);
+}
   // --^-- write or modify code above this line --^--
 }
 
