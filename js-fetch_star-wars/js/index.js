e8409f59b40ec7076d5da10b72bab7125d30a064
Margeaux Gebert <margeaux.gebert@gmail.com>
2025-02-24 11:19:33 +0100

    add solution star wars cards

--- js-fetch_star-wars/js/index.js
+++ js-fetch_star-wars/js/index.js
@@ -41,6 +41,15 @@ fetchDataAndRender();
 
 // --v-- your code below this line --v--
 
-function fetchDataAndRender() {
-  fetch(); // ?
+async function fetchDataAndRender() {
+  const response = await fetch("https://swapi.py4e.com/api/people"); // ?
+const data = await response.json();
+console.log(data.results);
+
+data.results.forEach(element => {
+  const newCards = Card(element);
+  renderElement(newCards);
+});
+
 }
+
