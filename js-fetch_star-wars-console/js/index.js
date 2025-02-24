b92dbe0af16e6082862538a4fd37e4da9d7f6e2e
Margeaux Gebert <margeaux.gebert@gmail.com>
2025-02-24 10:55:41 +0100

    add solution star wars console

--- js-fetch_star-wars-console/js/index.js
+++ js-fetch_star-wars-console/js/index.js
@@ -2,6 +2,17 @@ console.clear();
 
 const url = "https://swapi.py4e.com/api/people";
 
-function fetchData() {}
+async function fetchData() {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+  console.log(data.results);
+
+  const forthPerson = data.results[3].name;
+  console.log(forthPerson);
+
+  const eyeColor = data.results[2].eye_color;
+  console.log(eyeColor);
+}
 
 fetchData();
