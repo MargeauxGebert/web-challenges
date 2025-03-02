d095bbcfc783bf0f5624fec241932bc7541e0629
Margeaux Gebert <margeaux.gebert@gmail.com>
2025-03-02 22:15:25 +0100

    add solution buttons

--- react-props_buttons/src/App.jsx
+++ react-props_buttons/src/App.jsx
@@ -1,3 +1,16 @@
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  function handleClick() {
+    console.log("You clicked me! Now die.");
+  }
+  const textColor = "hotpink";
+  const textButton = "Click me, if you dare";
+  return <Button color="hotpink" text={textButton} onClick={handleClick} />;
+}
+
+function Button({ color, disabled, text, onClick }) {
+  return (
+    <button onClick={onClick} style={{ color: color }} disabled={disabled}>
+      {text}
+    </button>
+  );
 }
