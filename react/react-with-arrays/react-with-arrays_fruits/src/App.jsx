8ff88d2c641672f15c536bb63c1a812532984a9a
Margeaux Gebert <margeaux.gebert@gmail.com>
2025-03-04 10:36:44 +0100

    add fruit array to fruits

--- react/react-with-arrays/react-with-arrays_fruits/src/App.jsx
+++ react/react-with-arrays/react-with-arrays_fruits/src/App.jsx
@@ -1,7 +1,33 @@
 import Card from "./components/Card";
 
 export default function App() {
-  const fruits = [];
+  const fruits = [
+    {
+      id: 1,
+      name: "Mango 🥭",
+      color: "red-yellow",
+    },
+    {
+      id: 2,
+      name: "Pineapple 🍍",
+      color: "yellow-green",
+    },
+    {
+      id: 3,
+      name: "Blueberry 🫐",
+      color: "blue",
+    },
+    {
+      id: 4,
+      name: "Peach 🍑",
+      color: "peachy",
+    },
+    {
+      id: 5,
+      name: "Lemon 🍋",
+      color: "yellow",
+    },
+  ];
 
   return (
     <div className="app">
