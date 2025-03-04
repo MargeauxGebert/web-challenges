1157148e28ceeae081cf0ef0f339cee739dcb215
Margeaux Gebert <margeaux.gebert@gmail.com>
2025-03-04 11:39:11 +0100

    add solution users

--- react/react-with-arrays/react-with-arrays_users/src/components/Card.jsx
+++ react/react-with-arrays/react-with-arrays_users/src/components/Card.jsx
@@ -1,14 +1,15 @@
 import "./Card.css";
 import Tag from "./Tag";
 import Button from "./Button";
-function Card({ user }) {
+function Card({ name, roles, about, id }) {
   return (
-    <article className="card">
-      <h2>{user.name}</h2>
+    <article key={id} className="card">
+      <h2>{name}</h2>
       <ul className="card__taglist">
-        <Tag tag={user.roles[0]} />
+        {roles.map((role) =>
+          <Tag tag={role} />)}
       </ul>
-      <p>{user.about}</p>
+      <p>{about}</p>
       <Button>edit</Button>
       <Button buttonType="danger">invoice</Button>
     </article>
