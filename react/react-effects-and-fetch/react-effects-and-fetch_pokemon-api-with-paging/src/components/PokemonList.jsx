7f5ab6b1cf1c1cb73607e4b254fd00df03a59542
Margeaux Gebert <margeaux.gebert@gmail.com>
2025-03-05 15:17:43 +0100

    add solution pokemon paging

--- react/react-effects-and-fetch/react-effects-and-fetch_pokemon-api-with-paging/src/components/PokemonList.jsx
+++ react/react-effects-and-fetch/react-effects-and-fetch_pokemon-api-with-paging/src/components/PokemonList.jsx
@@ -2,12 +2,13 @@ import { useEffect, useState } from "react";
 
 export default function PokemonList() {
   const [pokemon, setPokemon] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
 
   useEffect(() => {
     async function loadPokemon() {
       try {
         const response = await fetch(
-          "https://pokeapi.co/api/v2/pokemon?offset=0"
+          `https://pokeapi.co/api/v2/pokemon?offset=${currentPage}`
         );
         const data = await response.json();
         setPokemon(data.results);
@@ -17,12 +18,21 @@ export default function PokemonList() {
     }
 
     loadPokemon();
-  }, []);
+  }, [currentPage]);
 
   return (
     <main>
-      <button type="button">Previous Page</button>
-      <button type="button">Next Page</button>
+      <button
+        type="button"
+        onClick={() =>
+          setCurrentPage(currentPage <= 0 ? null : currentPage - 20)
+        }
+      >
+        Previous Page
+      </button>
+      <button type="button" onClick={() => setCurrentPage(currentPage + 20)}>
+        Next Page
+      </button>
       <ul>
         {pokemon.map(({ name }) => (
           <li key={name}>{name}</li>
