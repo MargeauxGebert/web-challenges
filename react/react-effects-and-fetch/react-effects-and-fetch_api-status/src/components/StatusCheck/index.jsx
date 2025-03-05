d1eedbf4163fe44dbfa05ac5efd09d59817ea53d
Margeaux Gebert <margeaux.gebert@gmail.com>
2025-03-05 16:59:22 +0100

    add solution api status w/o bonus

--- react/react-effects-and-fetch/react-effects-and-fetch_api-status/src/components/StatusCheck/index.jsx
+++ react/react-effects-and-fetch/react-effects-and-fetch_api-status/src/components/StatusCheck/index.jsx
@@ -1,12 +1,14 @@
+import { useState } from "react";
 import "./StatusCheck.css";
 
 const apiStatusUrl = "https://example-apis.vercel.app/api/status";
 
 export default function StatusCheck() {
-  const statusIcon = "⁉️";
+  // const statusIcon = "⁉️";
+  const [statusIcon, setStatusIcon] = useState("⁉️");
   // Something needs to change here…
   // ↙️
-  function handleCheckApiStatus() {
+  async function handleCheckApiStatus() {
     /**
      * Hint 1:
      * Use the `fetch()` function to send a request to `apiStatusUrl`.
@@ -20,6 +22,14 @@ export default function StatusCheck() {
      * is okay and false if it is not.
      **/
     // --v-- write your code here --v--
+
+    const response = await fetch(apiStatusUrl);
+
+    if (!response.ok) {
+      return setStatusIcon("❌");
+    }
+    return setStatusIcon("✅");
+
     // --^-- write your code here --^--
   }
 
