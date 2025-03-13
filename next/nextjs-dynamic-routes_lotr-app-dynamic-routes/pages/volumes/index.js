14f3cfd15ca04a73eeaa908e76ce046cd8b42669
Margeaux Gebert <margeaux.gebert@gmail.com>
2025-03-13 11:26:45 +0100

    add solution dynamic-routes

--- next/nextjs-dynamic-routes_lotr-app-dynamic-routes/pages/volumes/index.js
+++ next/nextjs-dynamic-routes_lotr-app-dynamic-routes/pages/volumes/index.js
@@ -1,26 +1,35 @@
 import Link from "next/link";
 import { introduction } from "../../lib/data";
+import { volumes } from "../../lib/data";
+import { useRouter } from "next/router";
+import Head from "next/head";
 
 export default function Volumes() {
+  const router = useRouter();
+  function getRandomElement(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+
+  function handleRandomVolume() {
+    const randomIndex = getRandomElement(volumes).slug;
+    router.push(`/volumes/${randomIndex}`);
+  }
+
   return (
     <>
+    <Head>
+      <title>Volumes</title>
+    </Head>
       <h1>The Lord of the Rings</h1>
       <p>{introduction}</p>
       <ul>
-        <li>
-          <Link href="/volumes/the-fellowship-of-the-ring">
-            The Fellowship of the Ring
-          </Link>
-        </li>
-        <li>
-          <Link href="/volumes/the-two-towers">The Two Towers</Link>
-        </li>
-        <li>
-          <Link href="/volumes/the-return-of-the-king">
-            The Return of the King
-          </Link>
-        </li>
+        {volumes.map((volume) => (
+          <li key={volume.title}>
+            <Link href={`/volumes/${volume.slug}`}>{volume.title}</Link>
+          </li>
+        ))}
       </ul>
+      <button onClick={handleRandomVolume}>Random Volume</button>
     </>
   );
 }
