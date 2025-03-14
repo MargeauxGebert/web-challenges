e00c65770cae00d4c4dad5df5ea26d4d597bd17b
Margeaux Gebert <margeaux.gebert@gmail.com>
2025-03-14 17:28:26 +0100

    add solution till gameform

--- react/react-component-testing/react-component-testing_scorekeeper/components/Player/Player.test.js
+++ react/react-component-testing/react-component-testing_scorekeeper/components/Player/Player.test.js
@@ -2,6 +2,37 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Player from ".";
 
-test("renders player information and two buttons", () => {});
+test("renders player information and two buttons", () => {
+  render(<Player name="Mango" score={5} />);
 
-test("calls callbacks when increasing or decreasing score", async () => {});
+  const playerName = screen.getByText("Mango");
+  expect(playerName).toBeInTheDocument();
+
+  const playerScore = screen.getByText(5);
+  expect(playerScore).toBeInTheDocument();
+
+  const buttons = screen.getAllByRole("button");
+  expect(buttons).toHaveLength(2);
+});
+
+test("calls callbacks when increasing or decreasing score", async () => {
+  const Decrease = jest.fn();
+  const Increase = jest.fn();
+  const user = userEvent.setup();
+
+  render(
+    <Player onDecreasePlayerScore={Decrease} onIncreasePlayerScore={Increase} />
+  );
+
+  const buttonDecrease = screen.getByRole("button", { name: "Decrease Score" });
+
+  expect(Decrease).toHaveBeenCalledTimes(0);
+  await user.click(buttonDecrease);
+  expect(Decrease).toHaveBeenCalledTimes(1);
+
+  const buttonIncrease = screen.getByRole("button", { name: "Increase Score" });
+
+  expect(Increase).toHaveBeenCalledTimes(0);
+  await user.click(buttonIncrease);
+  expect(Increase).toHaveBeenCalledTimes(1);
+});
