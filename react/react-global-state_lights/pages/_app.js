ed2ed01a7c5a8a16704265c9911c646b0651419c
Margeaux Gebert <margeaux.gebert@gmail.com>
2025-03-17 14:09:11 +0100

    add some changes

--- react/react-global-state_lights/pages/_app.js
+++ react/react-global-state_lights/pages/_app.js
@@ -1,11 +1,62 @@
 import GlobalStyle from "../styles";
 import Layout from "../components/Layout";
+import { useState } from "react";
 
 export default function App({ Component, pageProps }) {
+const initialRooms = [
+  {
+    id: "1",
+    name: "Living Room",
+    isOn: false,
+  },
+  {
+    id: "2",
+    name: "Kitchen",
+    isOn: false,
+  },
+  {
+    id: "3",
+    name: "Bedroom",
+    isOn: false,
+  },
+  {
+    id: "4",
+    name: "Bathroom",
+    isOn: false,
+  },
+  {
+    id: "5",
+    name: "Garage",
+    isOn: false,
+  },
+  {
+    id: "6",
+    name: "Porch",
+    isOn: false,
+  },
+  {
+    id: "7",
+    name: "Garden",
+    isOn: false,
+  },
+  {
+    id: "8",
+    name: "Office",
+    isOn: false,
+  },
+];
+
+const [isOn, setIsOn] = useState(false);
+
+  function handleToggle() {
+    const newLight = initialRooms.filter((room) => room.id !== id)
+    setIsOn((isOn) => !isOn);
+  }
+
   return (
     <Layout>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <Component {...pageProps} initialRooms={initialRooms} isOn={isOn} onToggle={handleToggle}/>
     </Layout>
   );
 }
