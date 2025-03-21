30f465d01e7936b97d231719936a6aeb6f186c68
Margeaux Gebert <margeaux.gebert@gmail.com>
2025-03-21 17:13:06 +0100

    add solution random character

--- backend/backend-api-routes/backend-api-routes_random-character/pages/index.js
+++ backend/backend-api-routes/backend-api-routes_random-character/pages/index.js
@@ -1,3 +1,33 @@
+import useSWR from "swr";
+
+async function fetcher(url) {
+  const response = await fetch(url);
+  return await response.json();
+}
 export default function HomePage() {
-  return <h1>Hello from Next.js!</h1>;
+  const {
+    data: character,
+    isLoading,
+    error,
+  } = useSWR("/api/random-character", fetcher);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    return <p>404: Not found</p>;
+  }
+
+  return (
+    <main style={{ padding: "2rem" }}>
+      <p>
+        {character.prefix} {character.firstName} {character.lastName}
+      </p>
+      <p>Age: {character.age}</p>
+      <p>Twitter: {character.twitter}</p>
+      <p>Location: {character.location} (geohash)</p>
+      <p>Favourite Color: {character.color}</p>
+      <div style={{width: "100%", height: "50px", backgroundColor: `${character.color}`}}></div>
+    </main>
+  );
 }
