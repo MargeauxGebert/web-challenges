424c825aa689a06a2241d31f0c6c62c118dadd2c
Margeaux Gebert <margeaux.gebert@gmail.com>
2025-03-24 20:00:23 +0100

    add solution products

--- backend/backend-read/backend-read_products/pages/api/products/[id].js
+++ backend/backend-read/backend-read_products/pages/api/products/[id].js
@@ -1,14 +1,17 @@
-import { products } from "../../../lib/products";
+import Product from "@/db/models/Product";
+import dbConnect from "@/db/connect";
 
-export default function handler(request, response) {
+export default async function handler(request, response) {
   const { id } = request.query;
 
-  const product = products.find((product) => product.id === id);
+  await dbConnect();
 
-  if (!product) {
-    response.status(404).json({ status: "Not Found" });
+  if (request.method === "GET") {
+    const product = await Product.findById(id);
+    response.status(200).json(product);
+    return;
+  } else {
+    response.status(405).json({ message: "Method not allowed" });
     return;
   }
-
-  response.status(200).json(product);
 }
