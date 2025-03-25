3238aeb6dd1b29ed30d1add1474e28ce35db7728
Margeaux Gebert <margeaux.gebert@gmail.com>
2025-03-25 17:35:23 +0100

    add solution update

--- backend/backend-update-and-delete/backend-update-and-delete_products/components/ProductForm.js
+++ backend/backend-update-and-delete/backend-update-and-delete_products/components/ProductForm.js
@@ -1,57 +1,33 @@
 import styled from "styled-components";
-import useSWR from "swr";
 import StyledButton from "@/components/Button";
 
-export default function ProductForm() {
-  const { mutate } = useSWR("/api/products");
+const initialValue = {name: "", description: "", price: "", currency: "",}
 
-  async function handleSubmit(event) {
-    event.preventDefault();
-
-    const formData = new FormData(event.target);
-    const productData = Object.fromEntries(formData);
-
-    const response = await fetch("/api/products", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(productData),
-    });
-
-    if (!response.ok) {
-      console.error(response.status);
-      return;
-    }
-
-    mutate();
-    event.target.reset();
-  }
-
+export default function ProductForm({ onSubmit, isEditMode, value = initialValue }) { 
   return (
-    <StyledForm onSubmit={handleSubmit}>
-      <StyledHeading>Add a new Fish</StyledHeading>
+    <StyledForm onSubmit={onSubmit}>
+      <StyledHeading>{isEditMode ? `Edit ${value.name}` : "Add a new Fish"}</StyledHeading>
       <StyledLabel htmlFor="name">
         Name:
-        <input type="text" id="name" name="name" />
+        <input type="text" id="name" name="name" defaultValue={value.name} />
       </StyledLabel>
       <StyledLabel htmlFor="description">
         Description:
-        <input type="text" id="description" name="description" />
+        <input type="text" id="description" name="description" defaultValue={value.description} />
       </StyledLabel>
       <StyledLabel htmlFor="price">
         Price:
-        <input type="number" id="price" name="price" min="0" />
+        <input type="number" id="price" name="price" min="0" defaultValue={value.price}/>
       </StyledLabel>
       <StyledLabel htmlFor="currency">
         Currency:
-        <select id="currency" name="currency">
+        <select id="currency" name="currency" defaultValue={value.currency}>
           <option value="EUR">EUR</option>
           <option value="USD">USD</option>
           <option value="GBP">GBP</option>
         </select>
       </StyledLabel>
-      <StyledButton type="submit">Submit</StyledButton>
+      <StyledButton type="submit">{isEditMode ? `Save Edit` : "Add new Fish"}</StyledButton>
     </StyledForm>
   );
 }
--- backend/backend-update-and-delete/backend-update-and-delete_products/pages/[id].js
+++ backend/backend-update-and-delete/backend-update-and-delete_products/pages/[id].js
@@ -2,12 +2,15 @@ import useSWR from "swr";
 import styled from "styled-components";
 import { useRouter } from "next/router";
 import StyledLink from "@/components/Link";
+import { useState } from "react";
+import ProductForm from "@/components/ProductForm";
 
 export default function Product() {
+  const [isEditMode, setIsEditMode] = useState(false);
   const router = useRouter();
   const { id } = router.query;
 
-  const { data, isLoading } = useSWR(`/api/products/${id}`);
+  const { data, isLoading, mutate } = useSWR(`/api/products/${id}`);
 
   if (isLoading) {
     return <h1>Loading...</h1>;
@@ -17,15 +20,47 @@ export default function Product() {
     return;
   }
 
+  async function handleEditProduct(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const productData = Object.fromEntries(formData);
+    const response = await fetch(`/api/products/${id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(productData),
+    });
+    if (response.ok) {
+      mutate();
+    }
+    setIsEditMode(false);
+  }
+
   return (
-    <ProductCard>
-      <h2>{data.name}</h2>
-      <p>Description: {data.description}</p>
-      <p>
-        Price: {data.price} {data.currency}
-      </p>
-      <StyledLink href="/">Back to all</StyledLink>
-    </ProductCard>
+    <>
+      <button
+        type="button"
+        onClick={() => {
+          setIsEditMode(!isEditMode);
+        }}
+      >
+        Edit
+      </button>
+      {isEditMode && (
+        <ProductForm
+          onSubmit={handleEditProduct}
+          value={data}
+          isEditMode={true}
+        />
+      )}
+      <ProductCard>
+        <h2>{data.name}</h2>
+        <p>Description: {data.description}</p>
+        <p>
+          Price: {data.price} {data.currency}
+        </p>
+        <StyledLink href="/">Back to all</StyledLink>
+      </ProductCard>
+    </>
   );
 }
 
